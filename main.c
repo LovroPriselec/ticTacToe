2279491c47448e5aaa33a1b7e36bbcaa6936c287
Lovro Priselec <lpriselecbusiness@gmail.com>
2024-02-14 20:10:03 +0100

    main

--- main.c
+++ main.c
@@ -2,17 +2,30 @@
 #include <time.h>
 #include <stdlib.h>
 #include "search.h"
+#define LENGTH 9
 
 void print(int *polje)
 {
+    for (int i = 0; i < LENGTH / 3; i++)
+    {
+        for (int j = 0; j < LENGTH / 3; j++)
+        {
+            printf("%3d|", *(polje + LENGTH / 3 * i + j));
+        }
+        printf("\n------------");
+    }
     return;
 }
 
 int main(void)
 {
-    int array[9] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    int array[LENGTH] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
     srand((unsigned int)time(NULL));
-    int turn = rand() % (1-0+1)+0;
-    for
+    int turn = rand() % (1 - 0 + 1) + 0;
+    for (int i = 0; i < 10; i++)
+    {
+        printf("%d\n", turn);
+        turn = rand() % (1 - 0 + 1) + 0;
+    }
     return 0;
 }
--- search.c
+++ search.c
@@ -4,14 +4,17 @@
 
 _Bool searchRow(int *array)
 {
+    int res = 0;
 }
 
 _Bool searchColumn(int *array)
 {
+    int res = 0;
 }
 
 _Bool searchDiagonal(int *array)
 {
+    int res = 0;
 }
 
 _Bool search(int *array)
